5b21e64751548021517f3aa85ada976d894f1098
Vincent Winkler <vincentius.winkler@gmail.com>
2025-02-01 18:36:37 +0100

    wichtig

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -4,10 +4,10 @@
 
 #define _QWERTY 0
 #define _PROGRAM 1
-#define _NUM 2
+#define _NAV 2
 
-#define NUM MO(_NUM)
-#define PROGRAM MO(_PROGRAM)
+#define NAV MO(_NAV)
+#define PROGRAM OSL(_PROGRAM)
 
 
 
@@ -36,12 +36,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
     switch (keycode) {
         SMTD_MT(CKC_A, DE_A, KC_LEFT_GUI)
-        SMTD_MT(CKC_S, DE_S, KC_LEFT_ALT)
+        SMTD_LT(CKC_S, DE_S, _NAV)
         SMTD_MT(CKC_D, DE_D, KC_LEFT_CTRL)
         SMTD_MT(CKC_F, DE_F, KC_LSFT)
         SMTD_MT(CKC_J, DE_J, KC_LSFT)
         SMTD_MT(CKC_K, DE_K, KC_LEFT_CTRL)
-        SMTD_MT(CKC_L, DE_L, KC_LEFT_ALT)
+        SMTD_LT(CKC_L, DE_L, _NAV)
         SMTD_MT(CKC_OE, DE_ODIA, KC_LEFT_GUI)
     }
 }
@@ -88,7 +88,7 @@ enum combos {
 const uint16_t PROGMEM kl_combo[] = {CKC_K, CKC_L, COMBO_END};
 const uint16_t PROGMEM cw_togg[] = {KC_ENT, KC_BSPC, COMBO_END};
 const uint16_t PROGMEM ot_open_terminal[] = {DE_O, DE_T, COMBO_END};
-const uint16_t PROGMEM kw_kill_window[] = {DE_K, DE_W, COMBO_END};
+const uint16_t PROGMEM kw_kill_window[] = {CKC_K, DE_W, COMBO_END};
 const uint16_t PROGMEM workspace_1_combo[] = {KC_ENT, CKC_A, COMBO_END};
 const uint16_t PROGMEM workspace_2_combo[] = {KC_ENT, CKC_S, COMBO_END};
 const uint16_t PROGMEM workspace_3_combo[] = {KC_ENT, CKC_D, COMBO_END};
@@ -120,31 +120,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC , DE_1  , DE_2  , DE_3  , DE_4  , DE_5  ,                         DE_6  , DE_7  , DE_8  , DE_9  , DE_0  ,DE_SS,
         KC_TAB , DE_Q  , DE_W  , DE_E  , DE_R  , DE_T  ,                         DE_Z  , DE_U  , DE_I  , DE_O  , DE_P  ,DE_UDIA,
         KC_LSFT, CKC_A , CKC_S , CKC_D , CKC_F , DE_G  ,                         DE_H  , CKC_J , CKC_K , CKC_L ,CKC_OE ,DE_ADIA,
-        KC_LCTL, DE_Y  , DE_X  , DE_C  , DE_V  , DE_B  ,                         DE_N  , DE_M  ,DE_COMM,DE_DOT ,DE_SLSH,DE_BSLS,
+        KC_LCTL, DE_Y  , DE_X  , DE_C  , DE_V  , DE_B  ,                         DE_N  , DE_M  ,DE_COMM,DE_DOT ,DE_MINS,DE_SLSH,
                         KC_HOME,KC_END,                                                       _______, _______,
-                                         KC_BSPC,KC_SPC,                        KC_LSFT, KC_ENT,
+                                         KC_BSPC,KC_SPC,                        PROGRAM, KC_ENT,
                                          KC_TAB,KC_LCTL,                         PROGRAM,  KC_DEL,
                                          KC_TAB, KC_TAB,                        KC_LGUI, KC_LALT
     ),
 
+    // [_PROGRAM] = LAYOUT_5x6(
+    //     KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5 ,                         KC_F6  , KC_F7  , KC_F8  , KC_F9 , KC_F10 , KC_F11 ,
+    //     _______, _______, DE_EXLM, DE_QUES, DE_ASTR, DE_TILD,                        DE_DLR , DE_MINS, DE_UNDS, DE_AT , _______, _______,
+    //     _______, DE_PLUS, DE_LBRC, DE_LCBR, DE_LPRN, DE_QUOT,                        DE_DQUO, DE_RPRN, DE_RCBR, DE_RBRC, DE_EQL , _______,
+    //     _______, _______, DE_AMPR, DE_HASH ,DE_LABK, DE_GRV ,                        DE_PIPE, DE_RABK, DE_SCLN, _______, _______, _______,
+    //                       _______, _______,                                                            _______, _______,
+    //                                                 _______, _______,            _______, _______,
+    //                                                 _______, _______,            _______, _______,
+    //                                                 _______, _______,            _______, _______
+    // ),
+
     [_PROGRAM] = LAYOUT_5x6(
         KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5 ,                         KC_F6  , KC_F7  , KC_F8  , KC_F9 , KC_F10 , KC_F11 ,
-        _______, _______, DE_EXLM, DE_QUES, DE_ASTR, DE_TILD,                        DE_DLR , DE_MINS, DE_UNDS, DE_AT , _______, _______,
-        _______, DE_PLUS, DE_LBRC, DE_LCBR, DE_LPRN, DE_QUOT,                        DE_DQUO, DE_RPRN, DE_RCBR, DE_RBRC, DE_EQL , _______,
-        _______, _______, DE_AMPR, DE_HASH ,DE_LABK, DE_GRV ,                        DE_PIPE, DE_RABK, DE_SCLN, _______, _______, _______,
+        _______, DE_AMPR, DE_BSLS, DE_LCBR, DE_RCBR, DE_PERC,                        DE_QUOT, DE_LABK, DE_RABK, DE_QUOT, _______, _______,
+        _______, DE_PIPE, _______, DE_LPRN, DE_RPRN, DE_QUES,                        DE_EXLM, DE_MINS, DE_PLUS, DE_EQL , DE_HASH, _______,
+        _______, DE_TILD, DE_DLR , DE_LBRC ,DE_RBRC, DE_AT  ,                        DE_CIRC, DE_ASTR, DE_SCLN, DE_COLN, DE_UNDS, DE_SLSH,
                           _______, _______,                                                            _______, _______,
                                                     _______, _______,            _______, _______,
                                                     _______, _______,            _______, _______,
                                                     _______, _______,            _______, _______
     ),
 
-
-    [_NUM] = LAYOUT_5x6(
+    [_NAV] = LAYOUT_5x6(
         KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5 ,                         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-          _______,_______,_______,KC_UP,_______,_______,                    DE_ASTR,  DE_7 ,  DE_8 ,  DE_9,_______,_______,
-          _______,_______,KC_LEFT,KC_DOWN  ,KC_RGHT,_______,                DE_MINS,  DE_4 ,  DE_5 ,  DE_6 ,_______,_______,
-          _______,_______,_______,_______,_______,_______,                  DE_PLUS,  DE_1 ,  DE_2 ,  DE_3 ,_______,_______,
-                                                  _______,_______,            DE_0 ,_______,
+          _______,_______,DE_7   ,DE_8   ,DE_9   ,_______,                    _______,  _______ ,  _______ ,  _______,_______,_______,
+          _______,_______,DE_4   ,DE_5   ,DE_6   ,_______,                    _______,  KC_LEFT ,  KC_DOWN ,  KC_UP , KC_RIGHT,_______,
+          _______,_______,DE_1   ,DE_2   ,DE_3   ,_______,                    _______,  _______ ,  _______ ,  _______ ,_______,_______,
+                          _______,DE_0   ,                                              _______ , _______,
                                                   _______,_______,            _______,_______,
                                                   _______,_______,            _______,_______,
                                                   _______,_______,            _______,_______
